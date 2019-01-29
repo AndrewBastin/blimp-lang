d255e4e2e5a2851d3a1557b8e3d294490a9bda0a
Andrew Bastin <andrewbastin.k@gmail.com>
2019-01-29 11:19:40 +0530

    Fixed List is Empty exception when an empty string / comments are passed to the REPL

--- src/main/kotlin/blimp/syntax/closures/REPLBlock.kt
+++ src/main/kotlin/blimp/syntax/closures/REPLBlock.kt
@@ -4,6 +4,6 @@ import blimp.syntax.Node
 
 class REPLBlock(children: List<Node>): Block(children) {
 
-    override val canEvaluate = children.last().canEvaluate
+    override val canEvaluate = children.isNotEmpty() && children.last().canEvaluate
 
 }
